4df59d8640d725776b30aa66d01f7e281d4db2ca
Aerial LLC <110753825+e1uen@users.noreply.github.com>
2025-01-05 11:31:57 +0700

    Update Disable ShellExperienceHost (breaks action center).bat
    Signed-off-by: Aerial LLC <110753825+e1uen@users.noreply.github.com>

--- AerialOS/src/Post-Install/[3] Windows Settings/[2] Applications/App (UWP)/Disable ShellExperienceHost (breaks action center).bat
+++ AerialOS/src/Post-Install/[3] Windows Settings/[2] Applications/App (UWP)/Disable ShellExperienceHost (breaks action center).bat
@@ -1,7 +1,10 @@
 @echo off
+:: open please .bat in NSudo
 
+cd "%windir%\SystemApps\ShellExperienceHost_cw5n1h2txyewy"
 taskkill /F /IM ShellExperienceHost.exe
-
-"%windir%\NSudo.exe" -U:S -P:E cmd /c ren  "C:\Windows\SystemApps\ShellExperienceHost_cw5n1h2txyewy\ShellExperienceHost.exe" "ShellExperienceHost.exee"
+takeown /f ShellExperienceHost.exe
+icacls ShellExperienceHost.exe /grant Administrators:(F)
+rename ShellExperienceHost.exe ShellExperienceHost.exee
 
 exit
