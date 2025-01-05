2b169630e841cda192fbba3d7bd4c30ce91a8389
Aerial LLC <110753825+e1uen@users.noreply.github.com>
2025-01-05 11:30:55 +0700

    Update Disable SearchApp (breaks search).bat
    Signed-off-by: Aerial LLC <110753825+e1uen@users.noreply.github.com>

--- AerialOS/src/Post-Install/[3] Windows Settings/[2] Applications/App (UWP)/Disable SearchApp (breaks search).bat
+++ AerialOS/src/Post-Install/[3] Windows Settings/[2] Applications/App (UWP)/Disable SearchApp (breaks search).bat
@@ -1,12 +1,26 @@
 @echo off
+:: open please .bat in NSudo
 
 Reg.exe delete "HKLM\SOFTWARE\Classes\Folder\shell\pintohome" /f
 
 Reg.exe delete "HKCR\Folder\shellex\ContextMenuHandlers\PintoStartScreen" /f & Reg.exe delete "HKCR\exefile\shellex\ContextMenuHandlers\PintoStartScreen" /f
 Reg.exe delete "HKCR\Microsoft.Website\shellex\ContextMenuHandlers\PintoStartScreen" /f & Reg.exe delete "HKCR\mscfile\shellex\ContextMenuHandlers\PintoStartScreen" /f
 
-taskkill /F /IM SearchApp.exe & taskkill /F /IM TextInputHost.exe & taskkill /F /IM StartMenuExperienceHost.exe
+cd "%windir%\SystemApps\MicrosoftWindows.Client.CBS_cw5n1h2txyewy"
+taskkill /F /IM SearchHost.exe
+takeown /f SearchHost.exe
+icacls SearchHost.exe /grant Administrators:(F)
+rename SearchHost.exe SearchHost.exee
 
-"%windir%\NSudo.exe" -U:S -P:E cmd /c ren "C:\Windows\SystemApps\MicrosoftWindows.Client.CBS_cw5n1h2txyewy\SearchHost.exe" "SearchHost.exee" & "%windir%\NSudo.exe" -U:S -P:E cmd /c ren "C:\Windows\SystemApps\MicrosoftWindows.Client.CBS_cw5n1h2txyewy\TextInputHost.exe" "TextInputHost.exee" & "%windir%\NSudo.exe" -U:S -P:E cmd /c ren "C:\Windows\SystemApps\Microsoft.Windows.StartMenuExperienceHost_cw5n1h2txyewy\StartMenuExperienceHost.exe" "StartMenuExperienceHost.exee"
+taskkill /F /IM TextInputHost.exe
+takeown /f TextInputHost.exe
+icacls TextInputHost.exe /grant Administrators:(F)
+rename TextInputHost.exe TextInputHost.exee
+
+cd "%windir%\SystemApps\Microsoft.Windows.StartMenuExperienceHost_cw5n1h2txyewy"
+taskkill /F /IM StartMenuExperienceHost.exe
+takeown /f StartMenuExperienceHost.exe
+icacls StartMenuExperienceHost.exe /grant Administrators:(F)
+rename StartMenuExperienceHost.exe StartMenuExperienceHost.exee
 
 exit
