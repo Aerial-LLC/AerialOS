ccf40ea62c4805058fc487138e3c63a117b77c2d
Aerial LLC <110753825+e1uen@users.noreply.github.com>
2025-01-07 11:22:44 +0700

    Create dwm High.bat
    Signed-off-by: Aerial LLC <110753825+e1uen@users.noreply.github.com>

---
+++ AerialOS/src/Post-Install/[3] Windows Settings/[4] Registry Settings/Priority Threards/dwm/dwm High.bat
@@ -0,0 +1,6 @@
+@echo off
+
+reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Image File Execution Options\dwm.exe\PerfOptions" /v "CpuPriorityClass" /t REG_DWORD /d "3" /f >nul
+reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Image File Execution Options\dwm.exe\PerfOptions" /v "IoPriority" /t REG_DWORD /d "3" /f >nul
+
+exit
